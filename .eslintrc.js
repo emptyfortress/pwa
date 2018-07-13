afb7c39edbf78de16c53a19438c546e36cd30d84
Volkov Alexander <volkov.525@gmail.com>
2018-07-13 13:51:19 +0300

    my initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,20 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  }
+	root: true,
+	env: {
+		node: true
+	},
+	'extends': [
+		'plugin:vue/essential',
+		'@vue/standard'
+	],
+	"rules": {
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		"indent": [ "error", "tab" ],
+		"no-tabs": 0,
+		"no-mixed-spaces-and-tabs": [ "error", "smart-tabs"]
+	},
+	parserOptions: {
+		parser: 'babel-eslint'
+	}
 }
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  pwa: {
+    name: 'Docsvision',
+    themeColor: '#133C60'
+  }
+}
