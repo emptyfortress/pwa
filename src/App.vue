6a7d95e0b3562bf5a8d469115d0bc141e5ee5fb0
Volkov Alexander <volkov.525@gmail.com>
2018-07-18 16:15:40 +0300

    before experiment

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template lang="pug">
 v-app
-	v-navigation-drawer( persistent :mini-variant="miniVariant" :clipped="clipped" v-model="drawer" enable-resize-watcher fixed app )
+	v-navigation-drawer( v-model="drawer" floating disable-route-watcher fixed app :mini-variant="miniVariant" )
 		v-list
 			v-list-tile( value="true" v-for="(item, i) in items" :key="i" @click="navigate(item.to)")
 				v-list-tile-action
 					v-icon( v-html="item.icon" )
 				v-list-tile-content
 					v-list-tile-title( v-text="item.title" )
-	v-toolbar( app :clipped-left="clipped" flat scroll-off-screen scroll-threshold=100 )
+	v-toolbar( app flat clipped-left )
 		v-toolbar-side-icon( @click.stop="drawer = !drawer" )
 		v-toolbar-title( v-text="title" )
 		v-spacer/
@@ -16,22 +16,24 @@ v-app
 	v-content
 		v-slide-y-transition(mode="out-in")
 			router-view/
-	v-navigation-drawer( temporary :right="right" v-model="rightDrawer" fixed app )
-		v-list
-			v-list-tile( @click="right = !right" )
-				v-list-tile-action
-					v-icon compare_arrows
-				v-list-tile-title Switch drawer (click me)
-	v-bottom-nav( app :value="true" )
+	v-bottom-nav( fixed :value="true" ).hidden-sm-and-up
 		v-btn(flat color="teal")
 			span Recents
-				v-icon history
+			v-icon history
 		v-btn( flat color="teal" )
 			span Favorites
 			v-icon favorite
 		v-btn( flat color="teal" )
 			span Nearby
 			v-icon place
+	v-footer( app height=64 fixed clipped-left )
+		p laskjdalsk
+		<!-- v&#45;toolbar( flat ) -->
+		<!-- 	v&#45;toolbar&#45;side&#45;icon( @click.stop="drawer = !drawer" ) -->
+		<!-- 	v&#45;toolbar&#45;title( v&#45;text="title" ) -->
+		<!-- 	v&#45;spacer/ -->
+		<!-- 	v&#45;btn( icon @click.stop="rightDrawer = !rightDrawer" ) -->
+		<!-- 		v&#45;icon menu -->
 </template>
 
 <script>
@@ -43,6 +45,14 @@ export default {
 			clipped: true,
 			drawer: true,
 			fixed: true,
+			sheet: false,
+			tiles: [
+				{ img: 'keep.png', title: 'Keep' },
+				{ img: 'inbox.png', title: 'Inbox' },
+				{ img: 'hangouts.png', title: 'Hangouts' },
+				{ img: 'messenger.png', title: 'Messenger' },
+				{ img: 'google.png', title: 'Google+' }
+			],
 			items: [
 				{ icon: 'bubble_chart', title: 'Home', to: '/' },
 				{ icon: 'bubble_chart', title: 'Post a cat', to: '/post' }
@@ -61,3 +71,6 @@ export default {
 	}
 }
 </script>
+
+<style scoped lang="scss">
+</style>
