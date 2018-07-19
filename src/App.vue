e940e9efc349fef189e592fede607a11ad690dc5
Volkov Alexander <volkov.525@gmail.com>
2018-07-19 10:52:09 +0300

    seems cool

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,35 @@
 <template lang="pug">
 v-app
-	v-navigation-drawer( v-model="drawer" floating disable-route-watcher fixed app :mini-variant="miniVariant" )
+	v-navigation-drawer( v-model="drawer" clipped floating disable-route-watcher app :mini-variant="miniVariant" )
 		v-list
 			v-list-tile( value="true" v-for="(item, i) in items" :key="i" @click="navigate(item.to)")
 				v-list-tile-action
 					v-icon( v-html="item.icon" )
 				v-list-tile-content
 					v-list-tile-title( v-text="item.title" )
-	v-toolbar( app flat clipped-left )
+	v-toolbar( app flat clipped-left v-if="$vuetify.breakpoint.lgAndUp")
 		v-toolbar-side-icon( @click.stop="drawer = !drawer" )
 		v-toolbar-title( v-text="title" )
 		v-spacer/
 		v-btn( icon @click.stop="rightDrawer = !rightDrawer" )
 			v-icon menu
+	v-toolbar( app flat scroll-off-screen scroll-threshold=100 dark v-if="$vuetify.breakpoint.mdAndDown" )
+		v-toolbar-title( v-text="title" )
 	v-content
 		v-slide-y-transition(mode="out-in")
 			router-view/
-	v-bottom-nav( fixed :value="true" ).hidden-sm-and-up
-		v-btn(flat color="teal")
-			span Recents
-			v-icon history
-		v-btn( flat color="teal" )
-			span Favorites
-			v-icon favorite
-		v-btn( flat color="teal" )
-			span Nearby
-			v-icon place
-	v-footer( app height=64 fixed clipped-left )
-		p laskjdalsk
-		<!-- v&#45;toolbar( flat ) -->
-		<!-- 	v&#45;toolbar&#45;side&#45;icon( @click.stop="drawer = !drawer" ) -->
-		<!-- 	v&#45;toolbar&#45;title( v&#45;text="title" ) -->
-		<!-- 	v&#45;spacer/ -->
-		<!-- 	v&#45;btn( icon @click.stop="rightDrawer = !rightDrawer" ) -->
-		<!-- 		v&#45;icon menu -->
+	v-bottom-nav(app value="true" dark)
+		v-layout( row wrap )
+			v-flex
+				v-btn( flat color="teal" @click.stop="drawer = !drawer" )
+					v-icon menu
+			v-flex
+				v-btn( flat color="teal" @click.stop="drawer = !drawer" )
+					v-icon menu
+			v-spacer
+			v-flex
+				v-btn( flat color="teal" @click.stop="drawer = !drawer" )
+					v-icon menu
 </template>
 
 <script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,6 @@ v-container(grid-list-sm)
 		v-flex(v-for="picture in this.$root.cat" :key="picture.id" xs6 sm4 @click="displayDetails(picture['.key'])")
 			v-card(tile flat)
 				v-card-media(:src="picture.url" height="150")
-	footer
 </template>
 
 <script>
@@ -20,20 +19,24 @@ export default {
 	}
 }
 </script>
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-	font-weight: normal;
-}
-ul {
-	list-style-type: none;
-	padding: 0;
-}
-li {
-	display: inline-block;
-	margin: 0 10px;
-}
-a {
-	color: #42b983;
-}
+
+<style scoped lang="scss">
+
 </style>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- <style scoped> -->
+<!-- h1, h2 { -->
+<!-- 	font&#45;weight: normal; -->
+<!-- } -->
+<!-- ul { -->
+<!-- 	list&#45;style&#45;type: none; -->
+<!-- 	padding: 0; -->
+<!-- } -->
+<!-- li { -->
+<!-- 	display: inline&#45;block; -->
+<!-- 	margin: 0 10px; -->
+<!-- } -->
+<!-- a { -->
+<!-- 	color: #42b983; -->
+<!-- } -->
+<!-- </style> -->
