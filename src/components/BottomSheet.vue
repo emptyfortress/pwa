894781e176250da79613e47536fbd20bc2b1c295
Volkov Alexander <volkov.525@gmail.com>
2018-07-20 14:18:22 +0300

    fight with center paragraf in card

--- src/components/BottomSheet.vue
+++ src/components/BottomSheet.vue
@@ -4,14 +4,13 @@ v-bottom-sheet( v-model="sheet"  inset )
 		v-icon offline_bolt
 	v-list
 		v-container( grid-list-sm )
-			v-layout( row wrap )
+			v-layout( align-start wrap )
 				v-flex( v-for="tile in tiles" :key="tile.id" @click="sheet = false" )
 					v-card( tile flat )
-						v-card-media
-							v-avatar( size="64px" tile )
-								img( :src="`https://cdn.vuetifyjs.com/images/bottom-sheets/${tile.img}`" :alt="tile.title" )
+						v-card-media( height="64px" contain :src="`https://cdn.vuetifyjs.com/images/bottom-sheets/${tile.img}`")
 						v-card-title
-							h3.headline {{ tile.title }}
+							p.text-xs-center {{ tile.title }}
+							<!-- h3.headline.text&#45;xs&#45;center {{ tile.title }} -->
 </template>
 
 <script>
@@ -20,18 +19,18 @@ export default {
 	data () {
 		return {
 			inset: this.narrow,
-			sheet: false,
+			sheet: true,
 			tiles: [
 				{ id: 1, img: 'keep.png', title: 'Keep' },
 				{ id: 2, img: 'inbox.png', title: 'Inbox' },
 				{ id: 3, img: 'hangouts.png', title: 'Hangouts' },
 				{ id: 4, img: 'messenger.png', title: 'Messenger' },
-				{ id: 5, img: 'google.png', title: 'Google+' }
-				// { id: 6, img: 'keep.png', title: 'Keep' },
-				// { id: 7, img: 'inbox.png', title: 'Inbox' },
-				// { id: 8, img: 'hangouts.png', title: 'Hangouts' },
-				// { id: 9, img: 'messenger.png', title: 'Messenger' },
-				// { id: 10, img: 'google.png', title: 'Google+' }
+				{ id: 5, img: 'google.png', title: 'Google+' },
+				{ id: 6, img: 'keep.png', title: 'Keep' },
+				{ id: 7, img: 'inbox.png', title: 'Inbox' },
+				{ id: 8, img: 'hangouts.png', title: 'Hangouts' },
+				{ id: 9, img: 'messenger.png', title: 'Messenger' },
+				{ id: 10, img: 'google.png', title: 'Google+' }
 			]
 
 		}
