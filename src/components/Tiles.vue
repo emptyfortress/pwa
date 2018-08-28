8ecc93f8c29a30db7d8b725c39cd79e6eeb6d413
Volkov Alexander <volkov.525@gmail.com>
2018-08-28 16:32:26 +0300

    flip finished

--- src/components/Tiles.vue
+++ src/components/Tiles.vue
@@ -3,33 +3,37 @@ v-container(grid-list-xl fluid)
 	SlickList( :value="items" axis="xy" helperClass="moving" @input="newArr" )
 		v-layout( row wrap )
 			SlickItem(v-for="(item, index) in items" :index="index" :key="index" :item="item" ).sli
-				v-card(flat tile)
-					v-list-tile( avatar )
-						v-list-tile-avatar
-							img(src="@/assets/img/user0.svg").av
-						v-list-tile-content
-							v-list-tile-title {{ item.author }}
-							v-list-tile-sub-title 20 авг 13:10
-					v-divider
-					v-card-title(primary-title)
-						p.header {{item.title}}
-					p.descr Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-					v-card-actions
-						.status В работе
-						v-spacer
-						.attach
-							i.icon-skrepka
-								span 13
+				vue-flip( :active-click="true" width="100%" ).flip
+					v-card(flat tile slot="front" )
+						v-list-tile( avatar )
+							v-list-tile-avatar
+								img(src="@/assets/img/user0.svg").av
+							v-list-tile-content
+								v-list-tile-title {{ item.author }}
+								v-list-tile-sub-title 20 авг 13:10
+						v-divider
+						v-card-title(primary-title @click="doNothing")
+							p.header {{item.title}}
+						p.descr Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						v-card-actions
+							.status В работе
+							v-spacer
+							.attach
+								i.icon-skrepka
+									span 13
+					v-card(flat tile slot="back")
+						p alks als alsddk dkskjs skjs k
 </template>
 
 <script>
 import { SlickList, SlickItem } from 'vue-slicksort'
+import VueFlip from 'vue-flip'
 
 export default {
 	props: ['items'],
 	data () {
 		return {
-
+			activeOnClick: false
 		}
 	},
 	computed: {
@@ -37,11 +41,18 @@ export default {
 	methods: {
 		newArr (e) {
 			this.$store.commit('setItems', e)
+		},
+		handleFlip () {
+			this.activeOnClick = !this.activeOnClick
+		},
+		doNothing (evt) {
+			evt.stopPropagation()
 		}
 	},
 	components: {
 		SlickItem,
-		SlickList
+		SlickList,
+		VueFlip
 	}
 }
 </script>
@@ -60,6 +71,13 @@ export default {
 	margin: 1rem;
 }
 
+.flip {
+	width: 100%;
+	height: 350px;
+	/* padding: .5rem; */
+	/* overflow: hidden; */
+	/* position: relative; */
+}
 .v-card {
 	width: 100%;
 	height: 350px;
@@ -74,7 +92,6 @@ export default {
 	left: 0;
 	width: 100%;
 	height: 50px;
-	background: #fff;
 	padding: 0 1rem;
 }
 
@@ -83,10 +100,15 @@ export default {
 	box-shadow: 0 0 10px rgba(0,0,0,0.5);
 	font-family: Roboto;
 	color: #000;
+	line-height: 160%;
 }
 .header {
 	font-size: 1.2rem;
 	color: #82B1FF;
+	cursor: pointer;
+	&:hover {
+		text-decoration: underline;
+	}
 }
 .descr {
 	margin: 0 1rem;
