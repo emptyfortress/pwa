daaefb554b19bf4af2d078723725219a5c5bfc8e
Volkov Alexander <volkov.525@gmail.com>
2018-10-03 11:00:11 +0300

    continue

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -6,9 +6,15 @@ div
 			span Nutrition
 			v-spacer
 			v-text-field(v-model="search" append-icon="search" label="Search" single-line hide-details)
-		<v-data-table v-bind:headers="headers" :items="items" hide-actions class="elevation-1" ref="sortableTable" item-key="name" expand >
+		<v-data-table v-bind:headers="headers" :items="items" :search="search" hide-actions ref="sortableTable" item-key="name" expand >
 			<template slot="items" slot-scope="props">
-				<tr class="sortableRow" :key="itemKey(props.item)" @click="props.expanded = !props.expanded"> <!-- NOTE:  You'll need a unique ID, that is specific to the given item, for the key.   Not providing a unique key that's bound to the item object will break drag and drop sorting.  The itemKey method will return a uid for a given object using WeakMap.  You could just use a property in the object with a unique value, like "props.item.name" in this case, but often getting a unique value from the object's properties can be difficult, like when adding new rows, or when the unique field is open to editing, etc. -->
+				<!-- You'll need a unique ID, that is specific to the given item, for the key. -->
+				<!-- 	Not providing a unique key that's bound to the item object will break drag and drop sorting. -->
+				<!-- 	The itemKey method will return a uid for a given object using WeakMap. -->
+				<!-- 	You could just use a property in the object with a unique value, like "props.item.name" in this case, -->
+				<!-- 	but often getting a unique value from the object's properties can be difficult, like when adding new rows, -->
+				<!-- 	or when the unique field is open to editing, etc. -->
+				<tr class="sortableRow" :key="itemKey(props.item)" @click="props.expanded = !props.expanded">
 					<td class="px-1" style="width: 0.1%">
 						<v-btn style="cursor: move" icon class="sortHandle"><v-icon>drag_handle</v-icon></v-btn>
 					</td>
@@ -37,6 +43,7 @@ export default {
 	data () {
 		return {
 			expandRow: null,
+			search: '',
 			itemKeys: new WeakMap(),
 			currentItemKey: 0,
 			headers: [
@@ -46,7 +53,7 @@ export default {
 				{
 					text: 'Dessert (100g serving)',
 					align: 'left',
-					sortable: true,
+					sortable: false,
 					value: 'name'
 				},
 				{ text: 'Calories', value: 'calories', sortable: false },
