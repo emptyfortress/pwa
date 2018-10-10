91579d2b9b1f78eda8201fb8c658534a8abe2769
Volkov Alexander <volkov.525@gmail.com>
2018-10-10 13:28:30 +0300

    continue archive

--- src/views/Archive.vue
+++ src/views/Archive.vue
@@ -1,102 +1,37 @@
 <template lang="pug" >
-v-slide-x-transition(mode="out-in")
-	div
-		h2 Archive
-		<!-- v&#45;slide&#45;x&#45;reverse&#45;transition(mode="out&#45;in") -->
-		<!-- 	Tiles(v&#45;if="$vuetify.breakpoint.lgAndUp &#38;&#38; view === 'tile'" :items="items") -->
-		<!-- 	Split(v&#45;if="$vuetify.breakpoint.lgAndUp &#38;&#38; view === 'split'" :items="items") -->
-		<!-- 	DataTable(v&#45;if="$vuetify.breakpoint.lgAndUp &#38;&#38; view === 'table'" :items="items") -->
-		<!-- v&#45;layout( column v&#45;if="$vuetify.breakpoint.mdAndDown") -->
-		<!-- 	SlickList(lockAxis="y" :value="items" helperClass="moving" :pressDelay=300 @input="newArr") -->
-		<!-- 		SlickItem(v&#45;for="(item, index) in items" :index="index" :key="index" :item="item") -->
-		<!-- 			v&#45;card(flat :to="'/m/' + item.id" :class="item.unread ? 'unread' : ''" ).mobile -->
-		<!-- 				.wrap -->
-		<!-- 					.drag(@click.prevent="item.unread = !item.unread" ) -->
-		<!-- 					.card&#45;content -->
-		<!-- 						.head {{item.title}} -->
+div.all
+	.panel
+		v-slide-y-transition(mode="out-in")
+			v-flex(xs3 v-if="filterActive")
+				v-select(v-model="value" :items="items" label="Фильтр")
+		v-spacer
+		v-slide-y-transition(mode="out-in")
+			v-btn(flat v-if="filterActive" @click="filterActive = !filterActive" key='o') Применить фильтр
+			v-btn(flat v-if="!filterActive" @click="filterActive = !filterActive" key='a') Задать фильтр
+	v-slide-y-reverse-transition(mode="out-in")
+		v-layout(align-center justify-center column fill-height v-if="filterActive" key="one")
+			p {{ value }}
+		v-layout(align-center justify-center column fill-height v-if="!filterActive" key="two")
+			.arc
+				p Архив содержит миллионы документов.
+				p
+					i.icon-archive
+				p Задайте критерии фильтра
 
 </template>
 
 <script>
-import { SlickList, SlickItem, HandleDirective } from 'vue-slicksort'
-import { ResponsiveDirective } from 'vue-responsive-components'
-import Tiles from '@/components/Tiles'
-import Split from '@/components/Split'
-import DataTable from '@/components/DataTable'
-
 export default {
 	data () {
 		return {
-			cardResponse: {
-				tiny: el => el.width < 400,
-				small: el => el.width < 800,
-				big: el => el.width > 1000
-			},
-			selectMode: false,
-			detail: false,
-			selectAll: 'none',
-			selectNew: false
+			filterActive: false,
+			items: [ 'Дата', 'Тип документа', 'Автор', 'Исполнитель', 'Сотрудник', 'Статус', 'Вложения' ],
+			value: [ 'Дата', 'Тип документа', 'Автор', 'Исполнитель', 'Сотрудник', 'Статус', 'Вложения' ]
 		}
 	},
 	computed: {
-		view () { return this.$store.getters.view },
-		currentPath () { return this.currentFolder.path },
-		currentFolder () { return this.$store.getters.currentFolder },
-		loading () { return this.$store.getters.loading },
-		filter () { return this.currentFolder.filter },
-		selected () { return this.$store.getters.selected },
-		items () {
-			let all = this.$store.getters.items
-			if (this.filter === 'unread') {
-				return all.filter(item => item.unread)
-			} else if (this.filter === 'overdue') {
-				return all.filter(item => item.overdue)
-			} else if (this.filter === 'important') {
-				return all.filter(item => item.important)
-			} else if (this.selectNew === true) {
-				let unread = all.filter(item => item.unread)
-				unread.map(item => { item.selected = true })
-				return unread
-			} else if (this.selectNew === false) {
-				all.map(item => { item.selected = false })
-				return all
-			} else return all
-		}
 	},
 	methods: {
-		doNothing (evt) {
-			evt.stopPropagation()
-		},
-		featuredType (e) {
-			return this.featured.filter(x => x.type === e)
-		},
-		newArr (e) {
-			this.$store.commit('setItems', e)
-		},
-		myclass (e) {
-			let url = this.$route.params.id
-			url = parseInt(url, 10)
-			if (e.unread && e.id === url && !this.selectMode) return 'unread selected'
-			else if (e.unread) return 'unread'
-			else if (e.id === url && !this.selectMode) return 'selected'
-		},
-		toggleUnread (e) {
-			e.unread = !e.unread
-			this.$store.dispatch('updateItemReadStatus', e)
-		}
-	},
-	components: {
-		SlickItem,
-		SlickList,
-		Tiles,
-		Split,
-		DataTable
-		// DummyFolder,
-		// MultiSelect
-	},
-	directives: {
-		handle: HandleDirective,
-		responsive: ResponsiveDirective
 	}
 }
 </script>
@@ -104,45 +39,28 @@ export default {
 <style scoped lang="scss">
 @import '@/assets/css/colors.scss';
 
-.mt0 {
-	margin: 0;
-	margin-left: 2rem;
-}
-
-.v-avatar {
-	align-self: center;
-	margin: 0 .7rem;
-	.av {
-		background: $grey;
-	}
-}
-
-.wrap {
+.panel {
+	padding: .5rem 1rem;
+	/* border-bottom: 1px dotted #ccc; */
+	/* height: 3.8rem; */
+	/* width: 100%; */
 	display: flex;
+	/* background: #ccc; */
+}
+.arc {
+	color: $grey2;
+	text-align: center;
+	p {
+		font-size: 1.2rem;
+	}
+	i {
+		font-size: 10rem;
+		line-height: 1rem;
+		opacity: .7;
+	}
+}
+
+.all {
 	height: 100%;
-	.drag {
-		width: 8px;
-		background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAMAAAADCAYAAABWKLW/AAAAIUlEQVQYV2Ns2vj/f50/IyMDAwMDmPj///9/RhAAcWAAAN0pCAS0Z2yqAAAAAElFTkSuQmCC) repeat;
-	}
 }
-
-.v-card.unread, .v-card.selected.unread {
-	.drag {
-		background-color: $accent;
-	}
-	.head {
-		font-weight: bold;
-	}
-}
-
-.mobile.v-card {
-	.card-content {
-		margin: 1rem;
-		width: 100%;
-		.head {
-			font-size: 1.27rem;
-		}
-	}
-}
-
 </style>
