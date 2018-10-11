506bf1a8b8f23895ff3ac1bf1b018f6ece2d8b2c
Volkov Alexander <volkov.525@gmail.com>
2018-10-11 13:35:35 +0300

    continue

--- src/views/Archive.vue
+++ src/views/Archive.vue
@@ -1,29 +1,35 @@
 <template lang="pug" >
 div
 	.panel
-		v-layout( row wrap )
+		v-layout(row)
 			v-slide-y-transition(mode="out-in")
-				v-flex(xs2 v-if="state === 1" key='1')
-					v-select(v-model="e1" :items="e11" label="Фильтровать по").mx-3
+				v-layout( row v-if="state === 1" key='1')
+					v-flex
+						v-select(v-model="start" :items="filters" label="Фильтровать по").mx-3
+					v-slide-y-transition(mode="out-in")
+						v-flex(v-if="start === 'Дата создания'" key='lkj')
+							span date
+							<!-- v&#45;select(v&#45;model="second" multiple :items="value" label="Значение").mx&#45;3 -->
+						v-flex(v-else key='two')
+							v-select(v-model="second" multiple :items="value" label="Значение").mx-3
+					<!-- v&#45;flex -->
+					<!-- 	v&#45;icon calendar_today -->
+			v-spacer
 			v-slide-y-transition(mode="out-in")
-				v-layout(row wrap)
-					v-flex(xs4 v-if="state === 1 && e1 === 'Тип документа'" key='2')
-						v-select(v-model="e2" :items="e21" ).mx-3
-					v-flex(xs4 v-if="state === 1 && (e1 === 'Тип документа' || e1 === 'Дата')" key='3')
-						v-select(v-model="e3" :items="e31" ).mx-3
-			v-slide-y-transition(mode="out-in")
-				v-btn(flat v-if="state === 1" @click="state = 2" key='o') Применить фильтр
-				v-btn(flat v-if="state === 0" @click="state = 1" key='a') Задать фильтр
+				v-btn(flat v-if="state === 2" @click="state = 2" key='3') Применить фильтр
+				v-btn(flat v-if="state === 1" @click="state = 1" key='k4') Добавить фильтр
+				v-btn(flat v-if="state === 0" @click="state = 1" key='5') Задать фильтр
+
 	v-slide-y-reverse-transition(mode="out-in")
-		v-layout(align-center justify-center column fill-height v-if="(state === 1) && (e1 === '')" key="one").vcenter
+		v-layout(align-center justify-center column fill-height v-if="state === 1" key="one").vcenter
 			.zag За Последний месяц
 			v-layout( row wrap )
 				.butt(v-for="butt in buttons")
-					v-btn {{ butt }}
+					v-btn(flat) {{ butt }}
 			.zag.mt-5 За Последний квартал
 			v-layout( row wrap )
 				.butt(v-for="butt in buttons")
-					v-btn {{ butt }}
+					v-btn(flat) {{ butt }}
 
 		v-layout(align-center justify-center column fill-height v-if="state === 0" key="two").vcenter
 			.arc
@@ -41,16 +47,19 @@ export default {
 	data () {
 		return {
 			state: 0,
-			e1: '',
-			e11: [ 'Дата', 'Тип документа', 'Автор', 'Исполнитель', 'Сотрудник', 'Статус', 'Вложения' ],
-			e2: 'Задание',
-			e21: ['Задание', 'Групповое задание', 'Документ', 'Карточка', 'Договор', 'Служебная записка'],
+			start: '',
+			second: '',
+			doc: ['Задание', 'Групповое задание', 'Документ', 'Карточка', 'Договор', 'Служебная записка'],
+			srok: ['Последний месяц', 'Последний квартал', 'Текущий месяц', 'Текущий квартал'],
 			buttons: ['Я - автор', 'Я - исполнитель', 'Я - контролер', 'Договоры', 'Поручения', 'Служебки'],
-			e3: 'Последний месяц',
-			e31: ['Последний месяц', 'Последний квартал', 'Диапазон']
+			filters: [ 'Тип документа', 'Дата создания', 'Автор', 'Исполнитель', 'Сотрудник', 'Вложения' ]
 		}
 	},
 	computed: {
+		value () {
+			if (this.start === 'Тип документа') return this.doc
+			else if (this.start === 'Дата создания') return this.srok
+		}
 	},
 	methods: {
 	}
