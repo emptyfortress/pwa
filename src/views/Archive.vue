6d52bc4045c5fe989c98ab262e69f2851ea36f7a
Volkov Alexander <volkov.525@gmail.com>
2018-10-16 09:35:46 +0300

    made chips removable

--- src/views/Archive.vue
+++ src/views/Archive.vue
@@ -3,17 +3,17 @@ div
 	.panel
 		v-layout(row)
 			v-slide-y-transition(mode="out-in")
-				v-layout( row v-if="state === 1" key='1')
+				v-layout( row key='1')
 					v-flex(xs4)
 						v-select(v-model="start" :items="filters" label="Фильтровать по").mx-3
 					v-slide-y-transition(mode="out-in")
-						v-menu(v-if="start === 'Дата создания'" ref="menu" :close-on-content-click="false" v-model="menu" :nudge-right="30" :return-value.sync="date" lazy transition="scale-transition" offset-y full-width max-width="290px" min-width="290px")
+						v-menu(v-if="start === 'Дата создания'" ref="menu" :close-on-content-click="false" v-model="menu" :nudge-right="30" :return-value.sync="date" lazy transition="scale-transition" offset-y full-width max-width="290px" min-width="290px" key="five")
 							v-text-field(slot="activator" v-model="date" label="Год, месяц" prepend-icon="event" readonly)
 							v-date-picker(v-model="date" type="month" multiple scrollable locale="ru-ru")
 								v-spacer
 								v-btn(flat color="primary" @click="menu = false") Отмена
 								v-btn(flat color="primary" @click="$refs.menu.save(date)") OK
-						v-flex(v-if="start === 'Автор' || start === 'Исполнитель' || start === 'Сотрудник'" xs6 v-else key='three')
+						v-flex(v-if="start === 'Автор' || start === 'Исполнитель' || start === 'Сотрудник'" xs6 key='three')
 							v-autocomplete(:loading="personloading" :items="persons" :search-input.sync="search" v-model="select" solo multiple chips deletable-chips background-color="grey" cache-items class="mx-3" flat hide-no-data hide-details label="Искать в справочнике")
 								template( slot="item" slot-scope="{ item, tile }" )
 									v-list-tile-avatar
@@ -21,8 +21,7 @@ div
 									v-list-tile-content
 										v-list-tile-title {{ item }}
 										v-list-tile-sub-title.small отдел, департамент
-						<!-- v&#45;flex(xs7 v&#45;if="start === 'Тип документа' || start === 'Статус'" key='two') -->
-						v-flex(xs7 v-else key='two')
+						v-flex(xs7 v-if="start === 'Тип документа' || start === 'Статус' " key='two')
 							v-select(v-model="second" multiple :items="value" label="Значение").mx-3
 					v-slide-y-transition(mode="out-in")
 						v-flex(v-if="start !== ''" key="close")
@@ -30,31 +29,36 @@ div
 								v-icon close
 			v-spacer
 			v-slide-y-transition(mode="out-in")
-				<!-- v&#45;btn(flat v&#45;if="state === 2" @click="state = 2" key='3') Применить фильтр -->
-				v-btn(flat v-if="state === 1" @click="addFilter" key='k4') Добавить фильтр
-				v-btn(flat v-if="state === 0" @click="state = 1" key='5') Задать фильтр
+				v-btn(flat @click="addFilter" key='k4' :disabled="dis" ) Добавить фильтр
 
-	v-layout( row wrap v-if="filterlist" )
-		v-flex
-			v-chip(v-model="chip1" v-for="slot in slots" color="info" text-color="white" close) {{ slot }}
+	v-slide-y-transition(mode="out-in")
+		v-layout( row wrap v-if="state === 2" )
+			v-flex
+				v-chip(v-for="(slot, index) in slots" :key="index" @input="remove(slot)" color="info" text-color="white" close) {{ slot }}
+			v-spacer
+			v-btn(flat @click="state = 2" key='3') Применить фильтр
+
+	v-layout(align-center justify-center column v-if="state === 1" key="two")
+		.arc
+			p Архив содержит миллионы документов
+			p
+				i.icon-archive
+			p Задайте критерии фильтра
 
 	v-slide-y-reverse-transition(mode="out-in")
-		v-layout(align-center justify-center column fill-height v-if="start === '' && state === 1" key="one").vcenter
-			.zag За Последний месяц
-			v-layout( row wrap )
-				.butt(v-for="butt in buttons")
-					v-btn(flat) {{ butt }}
-			.zag.mt-5 За Последний квартал
-			v-layout( row wrap )
-				.butt(v-for="butt in buttons")
-					v-btn(flat) {{ butt }}
+		v-container
+			v-layout(column fill-height v-if="state === 1" key="one")
+				v-layout( row wrap align-center )
+					.zag
+						span За Последний месяц
+					.butt(v-for="butt in buttons")
+						v-btn(flat) {{ butt }}
+				v-layout( row wrap align-center)
+					.zag
+						span За Последний квартал
+					.butt(v-for="butt in buttons")
+						v-btn(flat) {{ butt }}
 
-		v-layout(align-center justify-center column fill-height v-if="state === 0" key="two").vcenter
-			.arc
-				p Архив содержит миллионы документов
-				p
-					i.icon-archive
-				p Задайте критерии фильтра
 </template>
 
 <script>
@@ -62,10 +66,7 @@ export default {
 	data () {
 		return {
 			chip1: true,
-			chip2: false,
-			chip3: false,
-			chip4: false,
-			state: 0,
+			state: 1,
 			menu: false,
 			date: null,
 			start: '',
@@ -341,6 +342,15 @@ export default {
 		}
 	},
 	computed: {
+		dis () {
+			if (this.second !== '') {
+				return false
+			} else if (this.date !== null) {
+				return false
+			} else if (this.select !== null) {
+				return false
+			} else return true
+		},
 		slot () {
 			let two = ''
 			if (this.second) {
@@ -359,15 +369,21 @@ export default {
 		}
 	},
 	methods: {
+		remove (item) {
+			this.slots.splice(this.slots.indexOf(item), 1)
+			this.slots = [...this.slots]
+		},
 		reset () {
 			this.start = ''
 			this.second = ''
+			this.date = null
+			this.select = null
 		},
 		addFilter () {
 			this.filterlist = true
 			this.slots.push(this.slot)
 			this.reset()
-			// console.log(123)
+			this.state = 2
 		},
 		querySelections (v) {
 			this.personloading = true
@@ -396,6 +412,7 @@ export default {
 .arc {
 	color: $grey2;
 	text-align: center;
+	margin-bottom: 3rem;
 	p {
 		font-size: 1.2rem;
 	}
@@ -405,13 +422,21 @@ export default {
 		opacity: .7;
 	}
 }
-.vcenter {
-	margin-top: 200px;
+.bottom {
+	position: absolute;
+	bottom: 20px;
+	left: 40%;
+	opacity: .5;
 }
 .zag {
 	text-transform: uppercase;
-	font-size: 2rem;
-	font-weight: 300;
+	font-size: .8rem;
+	span {
+		background: #999;
+		color: #fff;
+		padding: .5rem;
+	}
+	/* font-weight: 300; */
 }
 .application .theme--light.v-list .v-list__tile__mask, .theme--light .v-list .v-list__tile__mask {
 	color: #fff;
