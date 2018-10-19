c4683975e9982b9856c1d819bf4c075437df3df3
Volkov Alexander <volkov.525@gmail.com>
2018-10-19 15:51:23 +0300

    added templates on top

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -10,8 +10,36 @@ drag-it-dude(v-if="addTask" v-on:dblclick.native="expand" :class="assignClass")
 		v-layout(row fill-height justify-start ).add
 			div test
 			v-flex(xs12)
-				UserSelect(mycolor="white" label="Исполнитель" v-on:dblclick.native.stop v-model="selected")
-				p {{ selected }}
+				v-layout( row wrap )
+					v-btn(flat small color="accent" v-for="item in tem").temp
+						v-icon star
+						span {{ item }}
+					v-btn(flat small color="accent").temp
+						v-icon star_border
+						span 7
+
+				UserSelect(mycolor="white" label="Исполнители" v-on:dblclick.native.stop v-model="selected")
+				v-text-field(type='text' class="mx-3" label="Тема" v-model='theme' required )
+				v-text-field(type='text' class="mx-3 mt-0" label="Содержание" v-model='description' )
+				v-layout( row align-center class="mx-3" )
+					v-flex.rel
+						.label Дней на исполнение
+						v-layout(row align-center )
+							v-btn( flat icon @click="minus" )
+								v-icon remove
+							input(type="text" size="25" :value="days" id="count" placeholder="Дни")
+							v-btn( flat icon  @click="plus")
+								v-icon add
+					v-flex
+						v-menu(ref="menu"
+						:close-on-content-click="true"
+						v-model="menu" :nudge-right="30"
+						:return-value.sync="date" lazy transition="scale-transition"
+						offset-y full-width )
+							v-text-field(slot="activator" v-model="date" label="Дата исполнения" prepend-icon="event" readonly)
+							v-date-picker(v-model="date" @input="$refs.menu.save(date)" scrollable locale="ru-ru")
+				v-btn(flat) Файлы
+
 	.favusers
 		v-layout(row)
 			drag(v-for="n in 8" :key="n" class="drag" :transfer-data="{ draggable }" @mousedown.native.stop)
@@ -28,10 +56,20 @@ export default {
 		return {
 			expanded: 0,
 			selected: null,
-			draggable: 'Drag me'
+			date: '2018-11-12',
+			menu: null,
+			theme: null,
+			days: 3,
+			draggable: 'Drag me',
+			templ: [ 'Отчеты кв.', 'Новый договор', 'Закупка товаров', 'Цели подчиненных' ],
+			tem: [ 1, 2, 3, 4, 5, 6 ]
 		}
 	},
 	computed: {
+		srok () {
+			let today = new Date().toDateString()
+			return today
+		},
 		addTask () {
 			return this.$store.getters.addTask
 		},
@@ -41,9 +79,16 @@ export default {
 	},
 	components: {
 		DragItDude,
-		UserSelect
+		UserSelect,
+		VueNumericInput
 	},
 	methods: {
+		plus () {
+			this.days++
+		},
+		minus () {
+			this.days--
+		},
 		expand () {
 			if (this.expanded === 0) { this.expanded = 1 } else if (this.expanded === 1) { this.expanded = 2 } else if (this.expanded === 2) { this.expanded = 0 }
 		},
@@ -125,8 +170,30 @@ export default {
 
 .av {
 	background: $grey;
-	/* box-shadow: 0 0 3px 1px $info; */
-	/* border: 1px solid #000; */
+}
+
+#count {
+	font-size: 1.3rem;
+	text-align: center;
+	max-width: 50px;
+	height: 32px;
+}
+.increment {
+	font-size: 3rem;
+}
+.label {
+	font-size: .9rem;
+	position: absolute;
+	top: -10px;
+	left: 20px;
+	color: #666;
+}
+.rel {
+	position: relative;
+}
+.temp {
+	min-width: 0px;
+	margin: .5rem .1rem;
 }
 
 </style>
