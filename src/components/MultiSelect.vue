dc3080e994208c3f9b2e6aacbc958fc82a8efcd6
Volkov Alexander <volkov.525@gmail.com>
2019-03-11 15:13:09 +0300

    slider

--- src/components/MultiSelect.vue
+++ src/components/MultiSelect.vue
@@ -5,24 +5,34 @@
 			v-flex.text-xs-center
 				i.icon-basket
 				div Выберите объекты
-		v-layout(column  v-if="selected" key="two")
-			v-card.empty
-				v-window( v-model="onboarding" )
-					v-window-item( v-for="item in selItems" :key="item.id" )
-						div
-						<!-- v&#45;card( color="transparent" height="200" ) -->
-							v-layout( align-center justify-center fill-height tag="v-card-text" )
-								h3 {{ item.title }}
+		v-layout(column).mx-3
+			v-slider(v-model="size" min=200 max=1000 @input="changeWidth")
+			v-layout(row fill-height align-center  v-if="selected" key="two")
+				v-btn( icon @click="prev" )
+					i.icon-prev
+				div
+					v-card.empty
+						v-window( v-model="onboarding" )
+							v-window-item( v-for="item in selItems" :key="item.id" )
+								div
+									v-layout( align-center justify-center fill-height tag="v-card-text" )
+										.vert(v-bind:style="{width: computedWidth, height: computedHeight}")
+											img(:src="require('@/assets/img/docs/img' + item.id + '.jpg')" width="95%" v-if="item.files")
+											img(:src="require('@/assets/img/docs/empty.png')" width="95%" v-if="!item.files").empty
+											.emptyTitle(v-if="!item.files") {{ item.title }}
+										<!-- h3 {{ item.title }} -->
+				v-btn( icon @click="next")
+					i.icon-next
 
-				v-card-actions.justify-space-between
-					v-btn( icon @click="prev" )
-						i.icon-prev
-					v-item-group( v-model="onboarding" class="text-xs-center" mandatory )
-						v-item( v-for="n in length" :key="`btn-${n}`" )
-							v-btn( slot-scope="{ active, toggle }" :input-value="active" icon @click="toggle" )
-								v-icon mdi-record
-					v-btn( icon @click="next")
-						i.icon-next
+				<!-- v&#45;card&#45;actions.justify&#45;space&#45;between -->
+				<!-- 	v&#45;btn( icon @click="prev" ) -->
+				<!-- 		i.icon&#45;prev -->
+				<!-- 	v&#45;item&#45;group( v&#45;model="onboarding" class="text&#45;xs&#45;center" mandatory ) -->
+				<!-- 		v&#45;item( v&#45;for="n in length" :key="`btn&#45;${n}`" ) -->
+				<!-- 			v&#45;btn( slot&#45;scope="{ active, toggle }" :input&#45;value="active" icon @click="toggle" ) -->
+				<!-- 				v&#45;icon mdi&#45;record -->
+				<!-- 	v&#45;btn( icon @click="next") -->
+				<!-- 		i.icon&#45;next -->
 
 </template>
 
@@ -31,14 +41,27 @@ export default {
 	props: ['selItems'],
 	data () {
 		return {
+			size: 265,
+			width: 265,
+			height: 350,
 			// length: 3,
 			onboarding: 0
 		}
 	},
 	computed: {
-		selected () { return this.$store.getters.selected }
+		selected () { return this.$store.getters.selected },
+		computedWidth () {
+			return this.width + 'px'
+		},
+		computedHeight () {
+			return this.height + 'px'
+		},
 	},
 	methods: {
+		changeWidth () {
+			this.width = this.size
+			this.height = this.width * 1.4
+		},
 		next () {
 			this.onboarding = this.onboarding + 1 === length
 				? 0
@@ -76,20 +99,13 @@ export default {
 }
 
 .infolder {
-	padding-top: 0;
 	height: 100%;
 }
-.folder {
-	font-size: 2rem;
-	opacity: .5;
-	span {
-		margin-left: 1rem;
-	}
-}
+
 .empty {
-	height: calc(100vh - 200px);
-	width: calc((100vh - 200px)*0.71);
-	background: #fff;
+	/* height: calc(100vh - 160px); */
+	/* width: calc((100vh - 160px)*0.71); */
+	background: #ccc;
 	/* margin: 0 auto; */
 }
 
