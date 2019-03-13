5fbff074b9061c0010d3c0d9b885dccd7942fb8b
Volkov Alexander <volkov.525@gmail.com>
2019-03-13 13:13:51 +0300

    add control date

--- src/components/Attribute.vue
+++ src/components/Attribute.vue
@@ -2,6 +2,12 @@
 .cont
 	h2 {{ this.currentItem.title }}
 	.att1.mt-2
+		v-list-tile
+			v-list-tile-avatar
+				i.icon-deadline.deadline
+			v-list-tile-content
+				v-list-tile-sub-title срок
+				v-list-tile-title {{ currentItem.deadline }}
 		v-list-tile.mx-0
 			v-list-tile-avatar
 				img(:src="require('@/assets/img/user0.svg')").av
@@ -10,16 +16,22 @@
 				v-list-tile-title {{ currentItem.executor }}
 		v-list-tile
 			v-list-tile-avatar
+				i.icon-control-date.deadline
+			v-list-tile-content
+				v-list-tile-sub-title срок контроля
+				v-list-tile-title {{ currentItem.deadline }}
+		v-list-tile
+			v-list-tile-avatar.rel
 				img(:src="require('@/assets/img/user0.svg')").av
+				.k
+					span K
 			v-list-tile-content
 				v-list-tile-sub-title контролер
 				v-list-tile-title Петров А.П.
-		v-list-tile
-			v-list-tile-avatar
-				i.icon-deadline.deadline
-			v-list-tile-content
-				v-list-tile-sub-title срок
-				v-list-tile-title {{ currentItem.deadline }}
+	.descr.mt-3 {{ currentItem.descr }}
+	.attach.mt-4
+		i.icon-skrepka
+		span Файлы
 
 </template>
 
@@ -44,8 +56,27 @@ export default {
 .cont {
 }
 .av { background: #fff; }
+.k {
+	position: absolute;
+	top: -10px;
+	right: -5px;
+	span {
+		background: red;
+		font-size: .93rem;
+		padding: 0 .3rem;
+		border-radius: 20px;
+		color: #fff;
+		font-weight: 600;
+	}
+}
 .deadline {
 	font-size: 2rem;
+}
+.descr {
+	font-size: 1.2rem;
+}
+.attach {
+	font-size: 1.4rem;
 }
 h2 {
 	font-size: 2rem;
