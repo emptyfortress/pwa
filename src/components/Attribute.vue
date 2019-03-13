7c97bdcb397da391e4b2c92fe592d75fcd99704a
Volkov Alexander <volkov.525@gmail.com>
2019-03-13 14:31:55 +0300

    Some small fixes. Not too important

--- src/components/Attribute.vue
+++ src/components/Attribute.vue
@@ -1,58 +1,58 @@
 <template lang="pug">
-.cont
-	h2 {{ this.currentItem.title }}
-	.att1.mt-2
-		v-list-tile
-			v-list-tile-avatar
-				i.icon-deadline.deadline
-			v-list-tile-content
-				v-list-tile-title {{ currentItem.deadline }}
-				v-list-tile-sub-title срок
-		v-list-tile.mx-0
-			v-list-tile-avatar
-				img(:src="require('@/assets/img/user0.svg')").av
-			v-list-tile-content
-				v-list-tile-title {{ currentItem.executor }}
-				v-list-tile-sub-title автор
-		v-list-tile
-			v-list-tile-avatar
-				i.icon-control-date.deadline
-			v-list-tile-content
-				v-list-tile-title {{ currentItem.deadline }}
-				v-list-tile-sub-title срок контроля
-		v-list-tile
-			v-list-tile-avatar.rel
-				img(:src="require('@/assets/img/user0.svg')").av
-				.k
-					span K
-			v-list-tile-content
-				v-list-tile-title Петров А.П.
-				v-list-tile-sub-title контролер
-	.descr.mt-3 {{ currentItem.descr }}
-	.descr.mt-3(v-if="currentItem.files === undefined") Файлы отстуствуют
-
-	.attach.mt-4(v-if="currentItem.files")
-		i.icon-skrepka
-		span.mr-4 Файлы
-		span ({{ currentItem.files }})
-
-	.att1(v-if="currentItem.files")
-		v-list.files
-			v-list-tile( v-for="item in 3" :key="index" avatar @click="" )
-				v-list-avatar
-					img(:src="require('@/assets/img/filetype/file-doc.png')" )
+	.cont
+		h2 {{ this.currentItem.title }}
+		.att1.mt-2
+			v-list-tile
+				v-list-tile-avatar
+					i.icon-deadline.deadline
 				v-list-tile-content
-					v-list-tile-title this is title
-				v-list-tile-action
-					i.icon-next
-		v-list.files
-			v-list-tile( v-for="item in 3" :key="index" avatar @click="" )
-				v-list-avatar
-					img(:src="require('@/assets/img/filetype/file-doc.png')" )
+					v-list-tile-title {{ currentItem.deadline }}
+					v-list-tile-sub-title срок
+			v-list-tile.mx-0
+				v-list-tile-avatar
+					img(:src="require('@/assets/img/user0.svg')").av
 				v-list-tile-content
-					v-list-tile-title this is title laksjd  laskdj
-				v-list-tile-action
-					i.icon-next
+					v-list-tile-title {{ currentItem.executor }}
+					v-list-tile-sub-title автор
+			v-list-tile
+				v-list-tile-avatar
+					i.icon-control-date.deadline
+				v-list-tile-content
+					v-list-tile-title {{ currentItem.deadline }}
+					v-list-tile-sub-title срок контроля
+			v-list-tile
+				v-list-tile-avatar.rel
+					img(:src="require('@/assets/img/user0.svg')").av
+					.k
+						span K
+				v-list-tile-content
+					v-list-tile-title Петров А.П.
+					v-list-tile-sub-title контролер
+		.descr.mt-3 {{ currentItem.descr }}
+		.descr.mt-3(v-if="currentItem.files === undefined") Файлы отстуствуют
+
+		.attach.mt-4(v-if="currentItem.files")
+			i.icon-skrepka
+			span.mr-2 Файлы:
+			span {{ currentItem.files }}
+
+		.att1(v-if="currentItem.files")
+			v-list.files
+				v-list-tile( v-for="item in 3" :key="index" avatar @click="" )
+					v-list-avatar
+						img(:src="require('@/assets/img/filetype/file-doc.png')" )
+					v-list-tile-content
+						v-list-tile-title this is title
+					v-list-tile-action
+						i.icon-next
+			v-list.files
+				v-list-tile( v-for="item in 3" :key="index" avatar @click="" )
+					v-list-avatar
+						img(:src="require('@/assets/img/filetype/file-doc.png')" )
+					v-list-tile-content
+						v-list-tile-title this is title laksjd  laskdj
+						v-list-tile-action
+						i.icon-next
 
 </template>
 
