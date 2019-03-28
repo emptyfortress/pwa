692e8bdf2873a228713c6b3f4fb7e1182858ef95
Volkov Alexander <volkov.525@gmail.com>
2019-03-28 17:02:09 +0300

    change dashboard

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,44 +1,63 @@
 <template lang="pug">
 v-container(grid-list-md)
-	.display-1.font-weight-thin
-		i.icon-check
-		span Поручения
-	v-layout( column v-if="loading" align-center justify-center)
-		v-flex.mt-5
-			v-progress-circular( indeterminate color="primary" )
-	v-layout( row wrap v-if="!loading")
-		v-flex( xs12 sm6 )
-			v-card(flat :class="$vuetify.breakpoint.mdAndDown ? 'small' : 'big'")
-				v-layout( row justify-center align-center)
-					v-flex
-						vue-easy-pie-chart( :percent="75" :scale-length="0" :size="80")
-					v-flex
-						.folder Выполнено поручений на этой неделе
-		v-flex( xs12 sm6 )
-			v-card(flat :class="$vuetify.breakpoint.mdAndDown ? 'small' : 'big'")
-				v-layout( row justify-between align-center)
-					v-flex
-						vue-easy-pie-chart( :percent="0" :scale-length="0" :size="80")
-					v-flex
-						.folder Просрочено на этой неделе
-		v-flex( xs12 )
-			Widget(:folders="featuredType('task')")
-	br
-	br
-	br
-	br
-	.display-1.font-weight-thin
-		i.icon-doc
-		span Документы
-	Widget(:folders="featuredType('doc')")
-	br
-	br
-	br
-	br
-	.display-1.font-weight-thin
-		i.icon-folder
-		span Папки
-	Widget(:folders="featuredType('folder')")
+	v-layout(row)
+		v-flex(xs8)
+			.display-1.font-weight-thin
+				i.icon-check
+				span Поручения
+			v-layout( column v-if="loading" align-center justify-center)
+				v-flex.mt-5
+					v-progress-circular( indeterminate color="primary" )
+			v-layout( row wrap v-if="!loading")
+				v-flex( xs12 sm6 )
+					v-card(flat :class="$vuetify.breakpoint.mdAndDown ? 'small' : 'big'")
+						v-layout( row justify-center align-center)
+							v-flex
+								vue-easy-pie-chart( :percent="75" :scale-length="0" :size="80")
+							v-flex
+								.folder Выполнено поручений на этой неделе
+				v-flex( xs12 sm6 )
+					v-card(flat :class="$vuetify.breakpoint.mdAndDown ? 'small' : 'big'")
+						v-layout( row justify-between align-center)
+							v-flex
+								vue-easy-pie-chart( :percent="0" :scale-length="0" :size="80")
+							v-flex
+								.folder Просрочено на этой неделе
+				v-flex( xs12 )
+					Widget(:folders="featuredType('task')")
+			br
+			br
+			br
+			br
+			.display-1.font-weight-thin
+				i.icon-doc
+				span Документы
+			Widget(:folders="featuredType('doc')")
+			br
+			br
+			br
+			br
+			.display-1.font-weight-thin
+				i.icon-folder
+				span Папки
+			Widget(:folders="featuredType('folder')")
+		v-flex(xs4)
+			.one
+				.week Статистика недели (поручения)
+				.digit 14
+				apexchart( type="bar" :options="chartOptions" :series="series" id="bigChart")
+				br
+				.week Последние файлы
+				v-list(two-line).filelist
+					v-list-tile(v-for="file in files" :key="file.name" avatar @click="")
+						v-list-tile-avatar
+							i.icon-doc.lrg
+						v-list-tile-content
+							v-list-tile-title.bold {{ file.name }}
+							v-list-tile-subtitle.sub {{ file.size }}
+						v-list-tile-action
+							v-btn( icon )
+								v-icon(color="#003952") more_vert
 
 </template>
 
@@ -48,6 +67,50 @@ import 'vue-easy-pie-chart/dist/vue-easy-pie-chart.css'
 import Widget from '@/components/Widget'
 
 export default {
+	data () {
+		return {
+			files: [
+				{ name: 'Договор с ООО "Ромашка".doc', size: '45 kB' },
+				{ name: 'Приложение к договору.txt', size: '11 kB' },
+				{ name: 'Расписане выплат.xls', size: '23 kB' },
+				{ name: 'Приказ № 12-3/Л.doc', size: '15 kB' },
+			],
+			chartOptions: {
+				chart: {
+					toolbar: { show: false },
+					foreColor: '#fff',
+					colors: [ '#fff' ]
+				},
+				xaxis: {
+					categories: ['пн', 'вт', 'ср', 'чт', 'пт'],
+					position: 'bottom',
+					// labels: { offsetY: -18 },
+					axisBorder: { show: true, color: '#000' }
+				},
+				grid: { show: false },
+				colors: '#00567D',
+				fill: {
+					type: 'gradient',
+					gradient: {
+						shade: 'dark',
+						type: 'horizontal',
+						shadeIntensity: 0.25,
+						gradientToColors: undefined,
+						inverseColors: true,
+						opacityFrom: 0.85,
+						opacityTo: 0.85,
+						stops: [50, 0, 100]
+					}
+				}
+			},
+			plotOptions: {
+			},
+			series: [{
+				name: 'kmg01',
+				data: [5, 3, 1, 6, 2]
+			}]
+		}
+	},
 	computed: {
 		loading () {
 			return this.$store.getters.loading
@@ -71,6 +134,7 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import '@/assets/css/colors.scss';
 
 .container {
 	margin-bottom: 5rem;
@@ -112,6 +176,33 @@ export default {
 	svg {
 		height: 60px;
 	}
+}
+.one {
+	height: 96%;
+	/* margin-bottom: 6rem; */
+	margin-top: 3.4rem;
+	background: $info;
+	padding-top: 1rem;
+	.week {
+		/* text-align: center; */
+		color: #fff;
+		margin-left: 1rem;
+		margin-right: 1rem;
+		text-transform: uppercase;
+		font-size: 1.1rem;
+		border-bottom: 1px solid #ffffff66;
+	}
+	.digit {
+		margin-left: 1rem;
+		font-size: 3rem;
+		color: #fff;
+	}
+	.theme--light.v-list {
+		color: #fff;
+		background: transparent;
+	}
+	.lrg { font-size: 2.5rem; color: #003952; }
+	.sub { font-size: .9rem; color: #003952; }
 }
 
 </style>
